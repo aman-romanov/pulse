f21bb0c038aed4cc0065003044df51c4ed62265f
ouornek <byouornek@gmail.com>
2023-10-22 19:11:29 +0600

    added page scroll to up

--- src/js/script.js
+++ src/js/script.js
@@ -94,4 +94,19 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1500) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
   });
